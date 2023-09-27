3c646823538cece73543536c4a485f7f892fd59b
chewbacca234 <carine.dupuis.es@gmail.com>
2023-09-27 15:53:11 +0200

    correction of route fetch error

--- routes/movies.js
+++ routes/movies.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
-fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc&api_key=${TMDB_API_KEY}`)
-    .then(response => response.json())
-    .then(data => res.json({ result: true, movies: data.results }))
-    .catch(err => res.json({ result: false, err }));
+router.get('/', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc&api_key=${TMDB_API_KEY}`)
+        .then(response => response.json())
+        .then(data => res.json({result: true, movies: data.results}))
+        .catch(err => res.json({ result: false, err }));
+});
 
 module.exports = router;
