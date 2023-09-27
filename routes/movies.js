704932646732a9cf4f62d44a0cf60b35c00b83fa
chewbacca234 <carine.dupuis.es@gmail.com>
2023-09-27 15:33:05 +0200

    modify the API token

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-const TMDB_API_KEY = process.env.TMDB_API_KEY;
+const TMDB_API_READ_ACCES_TOKEN = process.env.TMDB_API_READ_ACCES_TOKEN;
 
 router.get('/', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc&api_key=${TMDB_API_KEY}`)
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer ' + TMDB_API_READ_ACCES_TOKEN
+        }
+    };
+
+    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc', options)
         .then(response => response.json())
-        .then(data => res.json({result: true, movies: data.results}))
+        .then(data => console.log('data', data))
         .catch(err => res.json({ result: false, err }));
 });
 
